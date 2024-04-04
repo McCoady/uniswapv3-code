d944da4d9fdd4807a4328af2fc1ff96ca8306718
McToady.eth <78250146+McCoady@users.noreply.github.com>
2024-04-04 13:39:11 +0900

    Update balanceOf signature
    balanceOf signature should be restricted to view so it uses a static call rather than a regular call to the contract (avoids potential malicious contracts who's balanceOf implementation can change state).

--- src/interfaces/IERC20.sol
+++ src/interfaces/IERC20.sol
@@ -6,7 +6,7 @@ interface IERC20 {
 
     function approve(address, uint256) external;
 
-    function balanceOf(address) external returns (uint256);
+    function balanceOf(address) external view returns (uint256);
 
     function transfer(address, uint256) external;
 
